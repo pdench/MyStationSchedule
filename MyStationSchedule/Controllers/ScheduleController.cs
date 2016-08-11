c4cad743c926bf32de695c90cd2f7418068b38e2
denchpa <denchpa@LTW73DPJYZ1>
2016-08-11 16:21:51 -0400

    Add project files.

---
+++ MyStationSchedule/Controllers/ScheduleController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+using Newtonsoft.Json;
+using System.Net;
+using MyStationSchedule.Models;
+
+namespace MyStationSchedule.Controllers
+{
+    public class ScheduleController : Controller
+    {
+        protected string serviceUrlBase = ConfigurationManager.AppSettings["serviceUrlBase"];
+        protected string apiKey = ConfigurationManager.AppSettings["api_key"];
+
+        // GET: Schedule
+        public ActionResult Index()
+        {
+            string url = serviceUrlBase + "routes?api_key=" + apiKey + "&format=json";
+            List<Routes> routeList = new List<Routes>();
+            WebClient client = new WebClient();
+            string output = client.DownloadString(url);
+
+            DateTime trainTime = FromUnixTime(1470922500);
+            Route routes = JsonConvert.DeserializeObject<Route>(output);
+            List<SelectListItem> routeListDD = new List<SelectListItem>();
+            foreach (var item in routes.mode)
+            {
+                if (item.route_type == "2")
+                {
+                    foreach (var thisRoute in item.route)
+                    {
+                        //routeList.Add(new Routes() { route_id = thisRoute.route_id, route_name = thisRoute.route_name });
+                        routeListDD.Add(new SelectListItem { Text = thisRoute.route_name, Value = thisRoute.route_id });
+                        //System.Diagnostics.Debug.WriteLine("id: {0}, name: {1}", thisRoute.route_id, thisRoute.route_name);
+                    }
+                }
+                
+            }
+
+            ViewData["routes"] = routeListDD;
+            return View(routeList);
+        }
+
+        public JsonResult GetStops(string id, string inOrOut)
+        {
+            string url = serviceUrlBase + "stopsbyroute?api_key=" + apiKey + "&route=" + id + "&format=json";
+            WebClient client = new WebClient();
+            string output = client.DownloadString(url);
+
+            List<SelectListItem> stopsList = new List<SelectListItem>();
+            StationStops stops = JsonConvert.DeserializeObject<StationStops>(output);
+            foreach (var item in stops.direction)
+            {
+                if (item.direction_id==inOrOut)
+                {
+                    foreach (var thisStop in item.stop)
+                    {
+                        System.Diagnostics.Debug.WriteLine(thisStop.parent_station_name);
+                        stopsList.Add(new SelectListItem { Text = thisStop.parent_station_name, Value = thisStop.stop_id });
+                    }
+                }
+            }
+            return Json(new SelectList(stopsList, "Text", "Value"));
+        }
+
+        public DateTime FromUnixTime(long unixTime)
+        {
+            var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            return epoch.AddSeconds(unixTime);
+        }
+    }
+}
---
+++ MyStationSchedule/Models/Route.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web;
+
+namespace MyStationSchedule.Models
+{
+
+
+    public class Routes
+    {
+        public string route_id { get; set; }
+        public string route_name { get; set; }
+        public string route_hide { get; set; }
+    }
+
+    public class Mode
+    {
+        public string route_type { get; set; }
+        public string mode_name { get; set; }
+        public IEnumerable<Routes> route { get; set; }
+    }
+
+    public class Route
+    {
+        public IEnumerable<Mode> mode { get; set; }
+    }
+
+    public class Stop
+    {
+        public string stop_order { get; set; }
+        public string stop_id { get; set; }
+        public string stop_name { get; set; }
+        public string parent_station { get; set; }
+        public string parent_station_name { get; set; }
+        public string stop_lat { get; set; }
+        public string stop_lon { get; set; }
+    }
+
+    public class Direction
+    {
+        public string direction_id { get; set; }
+        public string direction_name { get; set; }
+        public List<Stop> stop { get; set; }
+    }
+
+    public class StationStops
+    {
+        public List<Direction> direction { get; set; }
+    }
+}
